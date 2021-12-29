d6f7080304dc16b8e47c1733f4841e5c17aeb2ba
Adil Akhazzan <61242543+BlancoZX@users.noreply.github.com>
2021-12-29 19:20:28 +0100

    Añadimos una nueva persona

--- persona.cpp
+++ persona.cpp
@@ -7,11 +7,13 @@ int main(void)
 {
 	// create a new person named bob
 	Person bob;
-
+	Person b1;
 	// define bob
 	bob.setName("Bob");
 	bob.setAges(30);
 	bob.setNames("Ruben");
+	
+	
 	// display bob's age
 	std::cout << bob.name << " is " << bob.age << " years old." << std::endl;
 }
